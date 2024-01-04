99666b4cedf62ec79f0ae61168e9196ba4dae2e2
Oscar Diaz <oscargdi@hotmail.com>
2024-01-04 19:54:35 +0000

    Add dynamic port and host configuration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -10,6 +11,12 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(
+    configService.getOrThrow<number>('PORT'),
+    configService.getOrThrow<string>('HOST'),
+  );
 }
 bootstrap();
